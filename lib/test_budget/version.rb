5e5210144ba5451663cd13838c1fdc0c032b499f
Matheus Richard <matheusrichardt@gmail.com>
2026-03-05 15:02:21 -0300

    Let there be code!

---
+++ lib/test_budget/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module TestBudget
+  VERSION = "0.0.0"
+end
---
+++ spec/test_budget_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe TestBudget do
+  it "has a version number" do
+    expect(TestBudget::VERSION).not_to be nil
+  end
+end
